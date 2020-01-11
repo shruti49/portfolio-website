12d29cc890cd11a9e3318018fb92bc081fe18421
shruti <shrutishastri49@gmail.com>
2020-01-11 19:29:44 +0530

    adding animation

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/*Scroll to top when arrow up clicked BEGIN*/
+const topFunction = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+topFunction();
+new WOW().init();
